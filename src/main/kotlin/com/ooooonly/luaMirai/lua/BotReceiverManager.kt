dbd00218779a879f1c98bcb2aa645b153d285183
only52607 <1361822439@qq.com>
2020-08-27 11:21:55 +0800

    Update BotReceiverManager.kt

--- src/main/kotlin/com/ooooonly/luaMirai/lua/BotReceiverManager.kt
+++ src/main/kotlin/com/ooooonly/luaMirai/lua/BotReceiverManager.kt
@@ -2,6 +2,7 @@ package com.ooooonly.luaMirai.lua
 
 import net.mamoe.mirai.Bot
 
+//管理可接受bot推送的对象，以便在创建bot时将bot推送给对象
 object BotReceiverManager {
     private val receivers = mutableSetOf<BotReceivable>()
 
