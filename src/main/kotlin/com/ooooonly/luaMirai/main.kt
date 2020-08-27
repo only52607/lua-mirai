e0d4e1b1d5e3ddd6329cb489a055cdf81a89b533
only52607 <1361822439@qq.com>
2020-08-27 12:45:42 +0800

    Update main.kt

--- src/main/kotlin/com/ooooonly/luaMirai/main.kt
+++ src/main/kotlin/com/ooooonly/luaMirai/main.kt
@@ -1,33 +1,25 @@
 package com.ooooonly.luaMirai
 
-import com.ooooonly.luaMirai.lua.MiraiGlobals
-import kotlinx.coroutines.launch
+import com.ooooonly.luaMirai.lua.MiraiCoreGlobalsManger
+import com.ooooonly.luakt.runLuaFile
 import kotlinx.coroutines.runBlocking
 import net.mamoe.mirai.Bot
 import net.mamoe.mirai.join
-import org.luaj.vm2.Globals
-import org.luaj.vm2.LuaValue
 
 
 fun main(args: Array<String>) {
-    val globals: Globals = MiraiGlobals()
-    if (args.size == 0) {
-        println("请输入lua脚本路径！")
-        return
+    args.ifEmpty { throw Exception("请指定运行参数！") }
+    when (args[0]) {
+        "exec" -> if (args.size >= 2) execFile(args[1]) else throw Exception("请指定脚本路径！")
+        else -> throw Exception("未知的运行参数！")
     }
+}
 
-    val func: LuaValue = globals.loadfile(args[0])
-    if (func == LuaValue.NIL) {
-        println("脚本无效！")
-        return
-    }
-    func.call()
+fun execFile(filePath: String) {
+    runLuaFile(filePath, MiraiCoreGlobalsManger.getInstance())
     Bot.forEachInstance { bot ->
         runBlocking {
             bot.join()
         }
     }
 }
-
-
-
