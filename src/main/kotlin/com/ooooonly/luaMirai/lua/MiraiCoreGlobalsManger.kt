c751a6c48d47ee49313e0f2e24b8060e8288b3be
only52607 <1361822439@qq.com>
2020-08-28 11:48:05 +0800

    Update MiraiCoreGlobalsManger.kt

--- src/main/kotlin/com/ooooonly/luaMirai/lua/MiraiCoreGlobalsManger.kt
+++ src/main/kotlin/com/ooooonly/luaMirai/lua/MiraiCoreGlobalsManger.kt
@@ -3,17 +3,27 @@ package com.ooooonly.luaMirai.lua
 import com.ooooonly.luakt.asKValue
 import org.luaj.vm2.LuaClosure
 
-//管理Global容器以及内部产生的监听器，便于在销毁globals时将内部产生的监听器注销
+//通过ScriptId变量管理Global容器以及内部产生的监听器，便于在销毁globals时将内部产生的监听器注销
 object MiraiCoreGlobalsManger {
     private var globalsId = 0
     private var instances = mutableMapOf<Int, MiraiCoreGlobals>()
     private const val GLOBALS_ID = "_GLOBALS_ID"
+    private const val GLOBALS_NAME = "_GLOBALS_NAME"
 
-    fun checkGlobalsId(cls: LuaClosure): Int {
+    private fun checkGlobalsId(cls: LuaClosure): Int {
         val upvalues = cls.upValues
         if (upvalues.isEmpty()) return -1
         return upvalues[0].value[GLOBALS_ID].asKValue(-1)
     }
+
+    fun checkGlobalsName(cls: LuaClosure): String {
+        val upvalues = cls.upValues
+        if (upvalues.isEmpty()) return ""
+        return upvalues[0].value[GLOBALS_NAME].asKValue("")
+    }
+
+    fun checkGlobalsName(globals: MiraiCoreGlobals): String =
+        globals[GLOBALS_NAME].takeIf { it != null }?.asKValue("") ?: ""
 
     fun checkGlobals(cls: LuaClosure): MiraiCoreGlobals? {
         val id = checkGlobalsId(cls)
@@ -21,10 +31,11 @@ object MiraiCoreGlobalsManger {
         return getInstance(id)
     }
 
-    fun getInstance(id: Int = globalsId++): MiraiCoreGlobals =
+    fun getInstance(id: Int = globalsId++, name: String = ""): MiraiCoreGlobals =
         instances[id] ?: MiraiCoreGlobals().apply {
             instances[id] = this
             set(GLOBALS_ID, id)
+            set(GLOBALS_NAME, name)
         }
 
     fun removeInstance(id: Int) {
